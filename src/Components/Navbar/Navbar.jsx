756421d38259f7c3f18ec3585596897ae85603f4
Andrew Zhao <ninetailedfoxscp81124@gmail.com>
2025-09-01 08:39:18 -0400

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,18 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 function Navbar( {setPageTitle} ){
     const [sidebarIsOpen, setSidebarOpen] = useState(false);
     const handleSidebarToggle = () => setSidebarOpen(prev => !prev);
+    const handleLinkClick = (title) =>{
+        setPageTitle(title)
+        setSidebarOpen(false);
+    }
+
+    const navLinks = [
+        { path: "/about", title: "About"},
+        { path: "/contact", title: "Contact"},
+        { path: "/courses", title: "Courses"},
+        { path: "/donations", title: "Donations"},
+    ]
+
     const sidebarIcon = sidebarIsOpen ? <FaTimes /> : <FaBars />;
 
     return(
@@ -13,25 +25,27 @@ function Navbar( {setPageTitle} ){
         <div className="sidebar-wrapper"> {/* mobile compatability*/}
                     <div className={`sidebar-container ${sidebarIsOpen ? 'sidebar-open' : ''}`}>
                         <div className="sidebar-links">
-                            <Link className="Link" to="/about" onClick={() => setPageTitle("About")}><p className="link-bars">About</p></Link>
-                            <Link className="Link" to="/contact" onClick={() => setPageTitle("Contact")}><p className="link-bars">Contact</p></Link>
-                            <Link className="Link" to="/courses" onClick={() => setPageTitle("Courses")}><p className="link-bars">Courses</p></Link>                        
-                            <Link className="Link" to="/donations" onClick={() => setPageTitle("Donations")}><p className="link-bars">Donations</p></Link>
+                            {navLinks.map(({ path, title }) => (
+                                <Link key={title} className="Link" to={path} onClick={() => handleLinkClick(title)}>
+                                <p className="link-bars">{title}</p>
+                                </Link>
+                        ))}
                         </div>
                     </div>
                 </div>
 
             <div className="navbar-container">
-                <h1 onClick={handleSidebarToggle} className="sidebar-toggle">{sidebarIcon}</h1>
+                <button onClick={handleSidebarToggle} className="sidebar-toggle">{sidebarIcon}</button>
                 <div className="navbar-left">
-                    <Link to="/" onClick={() => setPageTitle("Home")} className="navbar-title"><h1><span className="navbar-title-emp-left">next</span> <span className="navbar-title-emp-right">Horizon</span></h1></Link>
+                    <Link to="/" onClick={() => handleLinkClick(title)} className="navbar-title"><h1><span className="navbar-title-emp-left">next</span> <span className="navbar-title-emp-right">Horizon</span></h1></Link>
                 </div>
                 <div className="navbar-right">
                     <div className="navbar-links"> 
-                        <Link className="Link" to="/about" onClick={() => setPageTitle("About")}><p>About</p></Link>
-                        <Link className="Link" to="/contact" onClick={() => setPageTitle("Contact")}><p>Contact</p></Link>
-                        <Link className="Link" to="/courses" onClick={() => setPageTitle("Courses")}><p>Courses</p></Link>
-                        <Link className="Link" to="/donations" onClick={() => setPageTitle("Donations")}><p>Donations</p></Link>
+                    {navLinks.map(({path, title}) => (
+                        <Link key={title} className="Link" to={path} onClick={() => setPageTitle({title})}>
+                        <p>{title}</p>
+                        </Link>
+                    ))}
                     </div>
                 </div>
             </div>
