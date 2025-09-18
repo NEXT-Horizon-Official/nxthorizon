5fcc0ed071aeaa297ba5a4fb31a366053293d688
Andrew Zhao <ninetailedfoxscp81124@gmail.com>
2025-09-18 18:46:51 -0400

    un

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,14 +6,14 @@ function Hero(){
         <>
         <div className="hero-container">
             <div className="hero-sub-one">
-                <h1 className="hero-text-primary">unlock your competetive <span className="emp-text-blue">coding</span> & <span className="emp-text-blue">math</span> potential</h1>
+                <h1 className="hero-text-primary">unlock your competitive <span className="emp-text-blue">coding</span> & <span className="emp-text-blue">math</span> potential</h1>
             </div>
             <div className="hero-sub-two">
-                <h1 className="hero-text-secondary">Empowering and inspiring the underrepresented to achieve their competetive coding and math goals and developing more oppurtunities in the field of STEM</h1>
+                <h1 className="hero-text-secondary">Empowering and inspiring the underrepresented to achieve their competitive coding and math goals and developing more opportunities in the field of STEM</h1>
             </div>
             <div className="hero-button-field">
                 <Link className="Link" to="/courses">
-                    <button>Start Learning</button>
+                    <button className="hero-start-button">Start Learning</button>
                 </Link>
                 <Link className="Link" to="/mission">
                     <button className="hero-learn-more-button">Learn More</button>
